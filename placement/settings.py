dda40bea109d0ce1d622782a76559d072ffe7acb
ansar <ansarmalappiuram6@gmail.com>
2025-11-12 14:45:50 +0530

    up

--- placement/settings.py
+++ placement/settings.py
@@ -28,11 +28,9 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-CORS_ALLOWED_ORIGINS = [
-    'https://placement-uyg9.onrender.com',
-]
+
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['https://placement-uyg9.onrender.com']
+
 
 # Application definition
 
@@ -48,7 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
